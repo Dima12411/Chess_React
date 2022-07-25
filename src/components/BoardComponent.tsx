da4a7e1a85fd31dfb449ceb2d755415a02c5fd11
Dima12411 <fixer40delta@gmail.com>
2022-07-25 07:42:32 +0300

    moving a chess figure

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -15,8 +15,7 @@ const BoardComponent: FC<BoardProps> = ({ board, setBoard }) => {
         if (selectedCell && selectedCell !== cell && selectedCell.figure?.canMove(cell)) {
             selectedCell.moveFigure(cell)
             setSelectedCell(null)
-        }
-        if (cell.figure) {
+        } else {
             setSelectedCell(cell)
         }
     }
