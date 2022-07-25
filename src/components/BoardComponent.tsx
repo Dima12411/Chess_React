6f3be6c2f22517acff61d11d3729eed25be6e164
Dima12411 <fixer40delta@gmail.com>
2022-07-25 08:59:38 +0300

    the transition of the move to the players in order

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -2,21 +2,28 @@ import React, { FC, useEffect, useState } from 'react';
 import { Board } from "../models/Board";
 import CellComponent from "./CellComponent";
 import { Cell } from "../models/Cell";
+import { Player } from "../models/Player";
 
 interface BoardProps {
     board: Board
     setBoard: (board: Board) => void
+    currentPlayer: Player | null
+    swapPlayer: () => void
 }
 
-const BoardComponent: FC<BoardProps> = ({ board, setBoard }) => {
+const BoardComponent: FC<BoardProps> = ({ board, setBoard, currentPlayer, swapPlayer }) => {
     const [selectedCell, setSelectedCell] = useState<Cell | null>(null)
 
     const click = (cell: Cell) => {
         if (selectedCell && selectedCell !== cell && selectedCell.figure?.canMove(cell)) {
             selectedCell.moveFigure(cell)
+            swapPlayer()
             setSelectedCell(null)
+            updateBoard()
         } else {
-            setSelectedCell(cell)
+            if (cell.figure?.color === currentPlayer?.color) {
+                setSelectedCell(cell)
+            }
         }
     }
 
@@ -43,7 +50,7 @@ const BoardComponent: FC<BoardProps> = ({ board, setBoard }) => {
                             cell={ cell }
                             key={ cell.id }
                             selected={ cell.x === selectedCell?.x && cell.y === selectedCell?.y }
-                            click={click}
+                            click={ click }
                         />
                     ) }
                 </React.Fragment>
